eb98e48be634e115ea8e6196b174a11fc532761c
pushpa-sree <vankadaripushpa123@gmail.com>
2026-09-24 22:11:52 +0530

    added file strucutre and multi clientn support with concurrentHashMap

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(6379);
+
+        System.out.println("MiniRedis server started on port 6379...");
+
+        ConcurrentHashMap<String, String> datastore =
+                new ConcurrentHashMap<>();
+
+        while (true) {
+
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println("Client connected!");
+
+            Thread clientThread = new Thread(() -> {
+
+                try {
+
+                    BufferedReader input =
+                            new BufferedReader(
+                                    new InputStreamReader(
+                                            clientSocket.getInputStream()
+                                    )
+                            );
+
+                    PrintWriter output =
+                            new PrintWriter(
+                                    clientSocket.getOutputStream(),
+                                    true
+                            );
+
+                    String message;
+
+                    while ((message = input.readLine()) != null) {
+
+                        if (message.equals("PING")) {
+
+                            output.println("PONG");
+
+                        } else if (message.startsWith("SET")) {
+
+                            String[] parts = message.split(" ", 3);
+
+                            String key = parts[1];
+                            String value = parts[2];
+
+                            datastore.put(key, value);
+
+                            output.println("OK");
+
+                        } else if (message.startsWith("GET")) {
+
+                            String[] parts = message.split(" ");
+
+                            String value = datastore.get(parts[1]);
+
+                            if (value != null) {
+                                output.println(value);
+                            } else {
+                                output.println("nil");
+                            }
+
+                        } else {
+
+                            output.println("ERR unknown command");
+                        }
+                    }
+
+                    clientSocket.close();
+
+                } catch (IOException e) {
+
+                    System.out.println("Client disconnected.");
+                }
+            });
+
+            clientThread.start();
+        }
+    }
+}
